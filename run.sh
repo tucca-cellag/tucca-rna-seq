6eb113e9978633981a0b5e6ec93ca2ad10744d10
benjibromberg <bromberg.benji@gmail.com>
2024-05-17 15:31:30 -0400

    update runner script

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 git pull
 module load miniconda/23.10
+source activate base
 source activate snakemake
 snakemake salmon_decoys \
   --cores 12 \
   --sdm conda \
+  --conda-frontend conda \
   -np --rerun-incomplete
