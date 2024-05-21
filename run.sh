f6129253921f0c95199d6f812295a5a9290759d9
benjibromberg <bromberg.benji@gmail.com>
2024-05-21 01:45:10 -0400

    refactor: Update run.sh to use salmon_index instead of salmon_decoys

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git pull
 module load miniconda/23.10
 source activate base
 source activate snakemake
-snakemake salmon_decoys \
+snakemake salmon_index \
   --workflow-profile profiles/slurm \
   -p --rerun-incomplete
