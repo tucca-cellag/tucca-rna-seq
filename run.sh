08d2e38a05336fb43b3da716dc340c668ad81b09
benjibromberg <bromberg.benji@gmail.com>
2024-05-21 17:10:21 -0400

    refactor: Update slurm_extra and error log paths in slurm config.yaml

--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Update the repository
 git pull
+
+# Create symlink to raw_data directory if not already made
+if [ ! -L data/raw_data ]; then
+  ln -s /cluster/tufts/kaplanlab/bbromb01/raw_data data/raw_data
+fi
+
+# Load conda
 module load miniconda/23.10
 source activate base
 source activate snakemake
+
+# Run snakemake pipeline
 snakemake star_index \
   --workflow-profile profiles/slurm \
-  -p --forcerun star_index
+  -np
