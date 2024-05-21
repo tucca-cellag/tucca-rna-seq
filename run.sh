7f26817befed79a5a3a4719d3d86bfa5bb348eb0
benjibromberg <bromberg.benji@gmail.com>
2024-05-21 00:39:34 -0400

    set resources & run salmon_decoys

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ git pull
 module load miniconda/23.10
 source activate base
 source activate snakemake
-snakemake unzip_genome \
+snakemake salmon_decoys \
   --workflow-profile profiles/slurm \
   -p --rerun-incomplete
