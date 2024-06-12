f06d171bbd06cf00184c4614ed61d731317eb564
benjibromberg <bromberg.benji@gmail.com>
2024-06-12 15:02:25 -0400

    refactor: Update run.sh to ensure latest changes are pulled

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 # Update the repository
 git pull
 
+# Update the repository again to ensure that the latest changes are pulled
+git pull
+
 # Create symlink to bbromb01/raw_data directory if not already made
 if [ ! -L data/raw_data ]; then
   ln -s /cluster/tufts/kaplanlab/bbromb01/raw_data data/raw_data
