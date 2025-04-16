f99913f2f2abdb10e70d24b8ab485529f3f1ebde
benjibromberg <bromberg.benji@gmail.com>
2025-04-16 14:37:08 -0400

    feat: add tximeta rule and script for processing salmon quantification results

---
+++ workflow/scripts/tximeta.R
@@ -0,0 +1,23 @@
+log <- file(snakemake@log[[1]], open = "wt")
+sink(log)
+sink(log, type = "message")
+date()
+suppressPackageStartupMessages({
+  library(devtools)
+})
+devtools::session_info()
+
+suppressPackageStartupMessages({
+  library(tximeta)
+})
+
+files <- file.path(list.files("results/salmon", full = TRUE), "quant.sf")
+all(file.exists(files))
+list.files("results/salmon")
+names <- list.files("results/salmon")
+names <- sub(".quant", "", names)
+names
+coldata <- data.frame(files, names)
+coldata
+
+saveRDS(coldata, file = snakemake@output[[1]])
