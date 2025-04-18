193072ff1f12bf3de44c9e50274a68ee2e627646
benjibromberg <bromberg.benji@gmail.com>
2025-04-18 18:18:03 -0400

    fix: modify tximeta call to use dynamic parameters

--- workflow/scripts/tximeta.R
+++ workflow/scripts/tximeta.R
@@ -22,11 +22,31 @@ names
 coldata <- data.frame(files, names)
 coldata
 
+extra <- snakemake@params[["extra"]]
+
 # Create summarized experiment using tximeta
-se <- tximeta::tximeta(coldata)
+# Evaluate the following
+se <- base::eval(
+  # ... parsed expression
+  base::parse(
+    # ... of tximeta and its arguments
+    text = base::paste0(
+      "tximeta::tximeta(", coldata, extra, ");"
+    )
+  )
+)
 
 ## Summarize to gene level
-gse <- summarizeToGene(se)
+gse <- summarizeToGene(
+  se,
+  assignRanges = "abundant",
+  countsFromAbundance = "lengthScaledTPM"
+)
+
+# Retrieve TxDb or EnsDb database
+db <- retrieveDb(se)
+
+class(db)
 
 saveRDS(se, file = snakemake@output[["se"]])
 saveRDS(gse, file = snakemake@output[["gse"]])
